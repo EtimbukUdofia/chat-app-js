3013926e8d2a1dd13a1c1bad7c25823dbdb37a42
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-07-10 14:26:41 +0100

    add sendMessage functionality/controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { config } from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-import connectDB from "./db/connectDB.js";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./db/connectDB.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 config();
 
@@ -13,8 +16,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 mongoose.connection.once("open", () => {
   console.log("Connected to mongoDB");
