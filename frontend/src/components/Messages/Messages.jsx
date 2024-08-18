aa8c6882b79247fb0216357da7993ab3b4dfa736
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-18 21:58:58 +0100

    make last message scroll into view

--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Message from './Message';
 import useGetMessages from '../../hooks/useGetMessages';
 import MessageSkeleton from '../skeletons/MessageSkeleton';
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
+
   console.log("messages:",messages);
   return (
-    <div className='px-4 flex-1 overflow-auto scroller'>
-      {!loading && messages.length > 0 && messages.map((message) => (
-        <Message key={message._id} message={ message } />
-      ))}
-
-      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}  {/* basically loop three times */}
-      
+    <div className="px-4 flex-1 overflow-auto scroller">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}{" "}
+      {/* basically loop three times */}
       {!loading && messages.length === 0 && (
-        <p className='text-center'>Send a message to start the conversation</p>
+        <p className="text-center">Send a message to start the conversation</p>
       )}
     </div>
-  )
+  );
 }
 
 export default Messages;
