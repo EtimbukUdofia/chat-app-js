f30455a597d07bb10a611241dfca1fc523abe8b7
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-18 22:00:55 +0100

    very minor fix

--- frontend/src/components/Messages/Message.jsx
+++ frontend/src/components/Messages/Message.jsx
@@ -20,7 +20,7 @@ const Message = ({message }) => {
         </div>
       </div>
 
-      <div className={`chat-bubble text-white ${bubbleBgColour}`}>
+      <div className={`chat-bubble text-white ${bubbleBgColour} pb-2`}>
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
